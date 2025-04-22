729ca8c557c9f231edf7f4e290aec3e769f2cca2
Raad <alirezzaraadco@gmail.com>
2025-04-23 01:10:47 +0330

    Created first method for WM that makes a PostgreSQL database and handled errors.

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # What is WM(WorkManager)?
+
+You can say I made this package for my life.
+
+I have a lot of time slippage in my life and time passes without me realizing what I was doing with my time,So I decided to use my knowledge and make a python package that I track my day with better precision and then make a telegram interface for to use this everywhere using a telegramBOT.
+
+____
--- WM/WM.py
+++ WM/WM.py
@@ -1,6 +1,53 @@
+import os
+
 import numpy as np
 import pandas as pd
+import psycopg2 as psql
+from dotenv import load_dotenv
+from psycopg2 import sql
+from psycopg2.errors import DuplicateDatabase
 
 
 class WorkManager:
-    pass
+    def __init__(self):
+        load_dotenv()
+        self.MakePsqlDB()
+
+    def MakePsqlDB(self) -> bool:
+        """Make a PostgresSql database based on .env "PSQ_*" parameters
+
+        Returns:
+            _type_: bool
+        """
+        conn_params = {
+            "dbname": "postgres",  # Connect to the default 'postgres' database
+            "user": os.environ["PSQL_USER"],
+            "password": os.environ["PSQL_PASSWORD"],  # Set to your password if required
+            "host": os.environ["PSQL_HOST"],
+            "port": os.environ["PSQL_PORT"],
+        }
+        try:
+            # Connect to the PostgreSQL server
+            conn = psql.connect(**conn_params)
+            conn.autocommit = True  # Enable autocommit for CREATE DATABASE
+
+            cursor = conn.cursor()
+
+            # Create the new database
+            cursor.execute(
+                sql.SQL("CREATE DATABASE {}").format(sql.Identifier("workmanager"))
+            )
+
+            print(f"Postgres Database initiated successfully!")
+            return True
+        except DuplicateDatabase:
+            print("Database is already initiated")
+            return True
+
+        except Exception as e:
+            print(f"An error has been occurred in database creation : {e}")
+            return False
+
+        finally:
+            cursor.close()
+            conn.close()
