d8c3560ccafc80ecd5514e09ce7a6a961974a44f
Raad <alirezzaraadco@gmail.com>
2025-04-25 22:33:04 +0330

    Added a connection pool and a cursor with context manager decorator to prevent constantly making a connection and cursor and closing it.

--- LM/LM.py
+++ LM/LM.py
@@ -1,4 +1,5 @@
 import os
+from contextlib import contextmanager
 
 import numpy as np
 import pandas as pd
@@ -6,21 +7,44 @@ import psycopg2 as psql
 from dotenv import load_dotenv
 from psycopg2 import sql
 from psycopg2.errors import DuplicateDatabase
+from psycopg2.pool import SimpleConnectionPool
 
 
 class LiferManager:
-    def __init__(self):
+    def __init__(self, minconn=1, maxconn=10):
         load_dotenv()
-        self.__config = {"psql_dbname": "workmanager",
-                         "psql_user": os.environ["PSQL_USER"],
-                         "psql_password": os.environ["PSQL_PASSWORD"],
-                         "psql_host": os.environ["PSQL_HOST"],
-                         "psql_port": os.environ["PSQL_PORT"]}
+        self.__config = {"dbname": "workmanager",
+                         "user": os.environ["PSQL_USER"],
+                         "password": os.environ["PSQL_PASSWORD"],
+                         "host": os.environ["PSQL_HOST"],
+                         "port": os.environ["PSQL_PORT"]}
+
         self.MakePsqlDB()
+
+        self._connection_pool = SimpleConnectionPool(
+            minconn=minconn, maxconn=maxconn, **self.__config)
 
     @property
     def config(self):
         return self.__config
+
+    @contextmanager
+    def __cursor(self):
+
+        conn = self._connection_pool.getconn()
+        cursor = conn.cursor()
+
+        try:
+            yield cursor
+            conn.commit()
+
+        except Exception as e:
+            conn.rollback()
+            raise e
+
+        finally:
+            cursor.close()
+            self._connection_pool.putconn(conn)
 
     def MakePsqlDB(self) -> bool:
         """Make a PostgresSql database based on .env "PSQ_*" parameters
@@ -28,12 +52,16 @@ class LiferManager:
         Returns:
             _type_: bool
         """
+        #! NOTE: I specifically DID NOT use connection pool for this one because I the dbname is set to postgres and
+        #! this method is a kick start for the database creation and What pool should give to a db that it isn't created
+        #! Yet! LOL
+
         conn_params = {
             "dbname": "postgres",  # Connect to the default 'postgres' database
-            "user": self.config["psql_user"],
-            "password": self.config["psql_password"],
-            "host": self.config["psql_host"],
-            "port": self.config["psql_port"],
+            "user": self.config["user"],
+            "password": self.config["password"],
+            "host": self.config["host"],
+            "port": self.config["port"],
         }
         try:
             # Connect to the PostgreSQL server
@@ -73,16 +101,22 @@ class LiferManager:
         Returns:
             bool: returns True if making task was successful
         """
-        conn_params = {
-            "dbname": self.config["psql_dbname"],
-            "user": self.config["psql_user"],
-            "password": self.config["psql_password"],
-            "host": self.config["psql_host"],
-            "port": self.config["psql_port"],
-        }
+
         try:
-            psql.connect(**conn_params)
+            with self.__cursor() as cursor:
+                print("Ye Buddy")
+
             return True
+
         except Exception as e:
+
             print(f"There was an error in TaskTable method -> {e}")
             return False
+
+    def _CreateTaskTable(self) -> bool:
+        """This Method makes Tasks Table in the database
+
+        Returns:
+            bool: True if it  successfully built it and False if it fails.
+        """
+        return False
