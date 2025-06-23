d35ff1378b691f8bccb35db226420efb2e4e1654
Raad <alirezzaraadco@gmail.com>
2025-06-23 09:42:18 +0330

    Wrote the whole insert task section.The st_LM module is finished; it remains further beautification.

--- LifeManager/LocalUI/st_LM.py
+++ LifeManager/LocalUI/st_LM.py
@@ -481,41 +481,184 @@ def insert_task():
             "You need a **Time** to Continue, Enter your time or you Start the Timer?"
         )
 
-    if st.checkbox(label="Custom Duration"):
-        custom_time = st.number_input(label="Enter You custom Time :")
-        custom_timeframe = st.selectbox(
-            label="Choose You'r TimeFrame", options=["Seconds", "Minutes", "Hours"]
+    # $ Bring up the duration tab.
+
+    if st.session_state.insert_duration is None:
+        st.markdown(
+            """<p style='font-size:24px;color:aqua'>Please Enter or Start your Timer for adding to the DB. </p>""",
+            unsafe_allow_html=True,
+        )
+        # Initialize state for enable/disable checkboxes
+        if "custom_duration_active" not in st.session_state:
+            st.session_state.custom_duration_active = False
+
+        if "timer_section_active" not in st.session_state:
+            st.session_state.timer_section_active = False
+
+        # define the functions that switch on/off
+        def enable_custom_duration():
+            st.session_state.custom_duration_active = True
+            st.session_state.timer_section_active = False
+
+        def enable_timer_section():
+            st.session_state.timer_section_active = True
+            st.session_state.custom_duration_active = False
+
+        user_entered_time = None
+
+        if st.checkbox(
+            "Custom Duration",
+            value=st.session_state.custom_duration_active,
+            key="custom_duration_checkbox",
+            on_change=enable_custom_duration,
+        ):
+            custom_time = st.number_input(label="Enter You custom Time :", step=1)
+            custom_timeframe = st.selectbox(
+                label="Choose You'r TimeFrame", options=["Seconds", "Minutes", "Hours"]
+            )
+            mapping_dict = {"Seconds": 1, "Minutes": 60, "Hours": 3600}
+            user_entered_time = custom_time * mapping_dict[custom_timeframe]
+            st.success(f"You Entered {user_entered_time:,.2f} Seconds...")
+
+        if st.checkbox(
+            "Timer Section",
+            value=st.session_state.timer_section_active,
+            key="timer_section_checkbox",
+            on_change=enable_timer_section,
+        ):
+            col1, col2, col3, col4 = st.columns(4)
+
+            with col1:
+                if st.button("Start"):
+
+                    tm.start()
+                    st.success("Timer Started")
+            with col2:
+                try:
+                    if st.button("Pause"):
+                        tm.pause()
+                        st.success("Timer Paused")
+                except:
+                    st.error("First initiate the start using **start** method")
+            with col3:
+                try:
+                    if st.button("Resume"):
+                        tm.resume()
+                        st.success("Timer Resumed")
+                except:
+                    st.error("Timer has not been paused")
+            with col4:
+                if st.button("End"):
+                    user_entered_time = tm.time_it()
+                    st.success(
+                        f"Timer Ended, Recorded Time {user_entered_time} Seconds..."
+                    )
+
+        if user_entered_time is not None:
+            st.button(
+                label=f"Save {user_entered_time:,.2f} Seconds....",
+                on_click=lambda: st.session_state.update(
+                    {"insert_duration": user_entered_time}
+                ),
+                type="primary",
+            )
+
+    # % The above if statement is for setting the value of insert_duration to a not-None value so this box below pop up.
+    if "insert_task" not in st.session_state:
+        st.session_state.insert_task = None
+
+    if (
+        st.session_state.insert_duration is not None
+        and st.session_state.insert_task is None
+    ):
+        st.markdown(
+            """<p style='font-size:24px;color:yellow'>Please Select Your Task: </p>""",
+            unsafe_allow_html=True,
         )
 
-    if st.checkbox("Timer Section"):
-        col1, col2, col3, col4 = st.columns(4)
+        prnt_tsk = st.selectbox(
+            label="Select Parent Task", options=lm.get_all_parent_tasks()
+        )
+        usr_task = st.selectbox(
+            label="Select Child Task",
+            options=lm.fetch_child_tasks_of(parent_task_name=prnt_tsk),
+        )
 
-        with col1:
-            if st.button("Start"):
-                tm.start()
+        st.button(
+            label=f"Confirm {usr_task}",
+            on_click=lambda: st.session_state.update({"insert_task": usr_task}),
+        )
 
-        with col2:
-            try:
-                if st.button("Pause"):
-                    tm.pause()
-            except:
-                st.error("First initiate the start using **start** method")
+    # $ Now its time for the user to add a description
+    if "insert_description" not in st.session_state:
+        st.session_state.insert_description = None
+    if (
+        st.session_state.insert_duration is not None
+        and st.session_state.insert_task is not None
+    ):
+        st.markdown(
+            """<p style='font-size:24px;color:lightgreen'>Enter Your Description of your work : </p>""",
+            unsafe_allow_html=True,
+        )
+        usr_dec = st.text_input(
+            label="Enter you Description",
+            help="Leave Empty if you dont want to use a description",
+            value="",
+        )
 
-        with col3:
-            try:
-                if st.button("Resume"):
-                    tm.resume()
-            except:
-                st.error("Timer has not been paused")
-        with col4:
+        st.button(
+            label="Confirm Description",
+            on_click=lambda: st.session_state.update({"insert_description": usr_dec}),
+        ),
 
-            if st.button("End"):
-                kh = tm.time_it()
-    try:
-        print(kh)
-        print(custom_time)
-    except:
-        pass
+    if (
+        st.session_state.insert_duration is not None
+        and st.session_state.insert_task is not None
+        and st.session_state.insert_description is not None
+    ):
+
+        def inserting(lm: LifeManager = lm):
+            desc = st.session_state.insert_description
+            print(bool(desc))
+            if lm.insert_into_weekly_table(
+                duration=st.session_state.insert_duration,
+                task_id=lm.fetch_task_id(st.session_state.insert_task),
+                description="null" if not bool(desc) else desc,
+            ):
+                st.success("Record Added Successfully.")
+            else:
+                st.error("The was an error while adding the Record.")
+
+        st.info(
+            f"""You are Adding the following to the database, please confirm:
+- Time : {st.session_state.insert_duration:,.2f} Seconds
+- Task : {st.session_state.insert_task} 
+- Description : {st.session_state.insert_description}"""
+        )
+
+        _col1, _col2 = st.columns(2)
+
+        with _col1:
+            if st.button("Add to the Database"):
+                inserting()
+                st.session_state.update(
+                    {
+                        "insert_duration": None,
+                        "insert_task": None,
+                        "insert_description": None,
+                    }
+                )
+        with _col2:
+            st.button(
+                "Insert Again",
+                on_click=lambda: st.session_state.update(
+                    {
+                        "insert_duration": None,
+                        "insert_task": None,
+                        "insert_description": None,
+                    }
+                ),
+            )
     st.markdown("<hr style='border: 1px solid yellow;'>", unsafe_allow_html=True)
     st.info("Click the button bellow to go to the MainPage:")
     st.button(
