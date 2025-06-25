08043dc13a5b5c5fa1e10e7e76ed2aa381a19471
Raad <alirezzaraadco@gmail.com>
2025-06-25 06:05:39 +0330

    Added Some text in st_home

--- LifeManager/LocalUI/st_home.py
+++ LifeManager/LocalUI/st_home.py
@@ -2,5 +2,10 @@ import streamlit as st
 
 
 def main():
-
-    st.title("Home Page")
+    st.header("Home Page", divider="rainbow")
+    st.markdown(
+        """
+        <p style='font-size:24px'><font color='Yellow'><b>Created By :</b></font> Alireza Raad</p>
+        <p style='font-size:24px;'><a href="https://raadlearn.com">Website</a> | <a href="https://github.com/AlirezaRaad/LifeManager">Github Page.</a></p>""",
+        unsafe_allow_html=True,
+    )
